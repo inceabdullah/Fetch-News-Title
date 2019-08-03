607bed6dd59dcf563746d7ee230014bca3b468f8
inceabdullah <pax.castrensis@gmail.com>
2019-08-03 02:34:37 -0700

    Update get_title_from_news.py

--- get_title_from_news.py
+++ get_title_from_news.py
@@ -5,7 +5,12 @@ from newspaper import Article
 def get_title_from_news(url):
     article = Article(url)
     article.download()
-    article.parse()
-    url_title = article.title
+    try:
+        article.parse()
+        url_title = article.title
+    except:
+        url_title = ""
+        
+    
     
     return url_title
